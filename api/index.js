ac465241641c953793a00aefd4cbaaf2f06d7d90
AlexaEunice <eunicealexah@gmail.com>
2025-07-04 15:16:31 +0800

    Delete api/index.js

--- api/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-app.use(express.json());
-
-// Load your Funcs
-require('./Funcs/GetStock.js')(app);
-require('./Funcs/GrabWeather.js')(app); // Add others the same way
-
-app.listen(3000, () => console.log('API LIVE 🚀'));
